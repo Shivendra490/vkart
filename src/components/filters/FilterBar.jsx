0845ff51b6b25aea3302a0b933ce9390308846c6
Shivendra490 <shivendrasrivastava490@gmail.com>
2023-09-25 16:16:14 +0530

    filters

--- src/components/filters/FilterBar.jsx
+++ src/components/filters/FilterBar.jsx
@@ -1,39 +1,63 @@
-import React from 'react'
-import styles from './FilterBar.module.css'
+import React, { useState } from "react";
+import styles from "./FilterBar.module.css";
 
 const FilterBar = () => {
+    const [isShowFilter,setIsShowFilter]=useState(true)
+    const removeFilter=()=>{
+        setIsShowFilter(false)
+    }
   return (
-    <div className={styles.filterWrapper}>
-        <div className={styles.searchWrapper}>
-            <input type='text'/>
+    <div className={`${styles.filterWrapper} ${isShowFilter ? styles.showFilter :styles.removeFilter}`}>
+        <div className={styles.cross}>
+            <div>Filters</div>
+            <div onClick={removeFilter}>*</div>
         </div>
-        <div className={styles.categoryWrapper}>
-            <select>
-                <option value='clothes'/>
-                <option value='furnitures'/>
-                
-            </select>
+      <div className={styles.searchWrapper}>
+        <input
+          className={styles.search}
+          type="text"
+          placeholder="Search Item"
+          name='search'
+        />
+      </div>
+      <div className={styles.categoryWrapper}>
+        <select name='category' className={styles.category}>
+          <option value="clothes">category</option>
+          <option value="furnitures">hk</option>
+        </select>
+      </div>
+      <div className={styles.ratingWrapper}>
+        <select name='rating' className={styles.category}>
+          <option value="clothes">rating</option>
+          <option value="furnitures">hk</option>
+        </select>
+      </div>
+      <div className={styles.minMaxWrapper}>
+        <select name='min' className={styles.category}>
+          <option value="clothes">min</option>
+          <option value="furnitures">hk</option>
+        </select>
+        <select name='max' className={styles.category}>
+          <option value="clothes">max</option>
+          <option value="furnitures">hk</option>
+        </select>
+      </div>
+      <div className={styles.sortWrapper}>
+        <div>Sort : </div>
+        <div className={styles.label}>
+          <span>Low to high</span>
+          <input type="radio" name="sort" />
         </div>
-        <div className={styles.ratingWrapper}>
-            <select>
-                <option value='clothes'/>
-                <option value='furnitures'/>
-                
-            </select>
+        <div className={styles.label}>
+          <span>High to Low</span>
+          <input type="radio" name="sort" />
         </div>
-        <div className={styles.minMaxWrapper}>
-                minmax
-        </div>
-        <div className={styles.sortWrapper}>
-            sort
-        </div>
-        <div className={styles.resetWrapper}>
-            <button>Reset</button>
-        </div>
-        
-      
+      </div>
+      <div className={styles.resetWrapper}>
+        <button className={styles.reset}>Reset Filters</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FilterBar
+export default FilterBar;
